0f63683f3f444769071643ff28453fff814d36d9
viiictoriiia <122220829+viiictoriiia@users.noreply.github.com>
2023-01-10 19:01:41 +0500

    Update wish.py

---
+++ wish.py
@@ -0,0 +1,5 @@
+import random
+
+new_year_wishes = ['May All Your Dreams Come True!', 'May you have an ocean of happiness!', 'May this Christmas be bright and cheerful and may the New Year begin on a prosperous note!', 'Happy 2023 To You!', 'A health to you, a wealth to you and the best that life can give to you!']
+
+print(random.choice(new_year_wishes))
